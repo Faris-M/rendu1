a21bca9eec94d8fe87fb13328468f19c5fb2e06e
Faris-M <mouwaffakfaris@gmail.com>
2020-01-14 19:13:16 +0100

    exo6 7 et 8

---
+++ exo_07_a.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+p user_name
+=begin 
+"gets.chomp" : "gets" demande texte entré par utilisateur
+chomp efface ligne suivante créée
+=end
