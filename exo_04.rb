e02d75533af0cc1e98f2f1a20c13d00e19863607
Faris-M <mouwaffakfaris@gmail.com>
2020-01-14 17:26:45 +0100

    exo4 et exo5

---
+++ exo_04.rb
@@ -0,0 +1,3 @@
+puts "Salut, ça farte ?"
+
+# Cela ne fonctionnait pas car il manquait le " finissant l'argument 
---
+++ exo_05.rb
@@ -0,0 +1,28 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+=begin #{} est un opérateur utilisé pour afficher un calcul executé dans le string;
+dans les lignes 2,3,13,14 la syntaxe #{} permet dafficher les calculs
+Les lignes 16 et 18 affichent seulement la chaine de caractères entre string
+dans les lignes 20,21,22 il sagit des booleens et en fonction du résultat la valeur true ou false est affichée 
+
+=end
