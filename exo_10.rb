4c2f76ae4f65c93b0d31f30004be2108e4ef38ac
Faris-M <mouwaffakfaris@gmail.com>
2020-01-14 19:52:12 +0100

    exo10

---
+++ exo_10.rb
@@ -0,0 +1,14 @@
+puts "Quel est votre année de naissance"
+
+user_born_year = gets.chomp.to_i
+
+age = 2017 - user_born_year
+
+
+puts "Votre aviez #{age} ans en 2017 !"
+
+
+=begin
+Programme demandant l'année de naissance de l'utilisateur et lui
+renvoi l'age qu'il a eu en 2017
+=end
